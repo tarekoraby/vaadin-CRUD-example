addb353adb33b3a6bd1da1bd4e3fee3fa9a0f9ee
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2020-02-27 11:15:24 +0200

    Implement a workaround to undesirable editor clear

--- src/main/java/com/example/test/MainView.java
+++ src/main/java/com/example/test/MainView.java
@@ -12,38 +12,42 @@ import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.Grid.SelectionMode;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.data.selection.MultiSelect;
 import com.vaadin.flow.router.Route;
 
-
 @Route("")
 public class MainView extends VerticalLayout {
 
 	Grid<Person> childrenGrid;
-	VerticalLayout form;
-	Dialog dialog;
+	boolean errorProne;
 
 	public MainView() {
 
 		final ParentDataProvider dataProvider = new ParentDataProvider();
 		setupChildrenGrid(dataProvider);
 
-		final Crud<Parent> crud = new Crud<>(Parent.class, createParentEditor());		
-		
+		final Crud<Parent> crud = new Crud<>(Parent.class, createParentEditor());
+
 		crud.setDataProvider(dataProvider);
+
+		// Setting errorProne to true ensures that only when the editor is being
+		// opened could the validation fail
+		crud.addEditListener(e -> errorProne = true);
+
+		// Otherwise (i.e. if the editor is being closed due to save, delete, or
+		// cancel) the validation will always pass due to errorProne = false
 		crud.addSaveListener(e -> {
 			dataProvider.persist(e.getItem());
-			dialog.close();
-			if(dialog.getParent().isPresent())
-				dialog.getParent().ifPresent(p -> System.out.println(p.toString()));
-			else 
-				System.out.println("Dialog has no parent");
+			errorProne = false;
 		});
-		crud.addDeleteListener(e -> dataProvider.delete(e.getItem()));
+		crud.addDeleteListener(e -> {
+			dataProvider.delete(e.getItem());
+			errorProne = false;
+		});
+		crud.addCancelListener(e -> errorProne = false);	
 
 		crud.setEditorPosition(CrudEditorPosition.ASIDE);
 		crud.setEditOnClick(true);
@@ -56,23 +60,33 @@ public class MainView extends VerticalLayout {
 
 	private CrudEditor<Parent> createParentEditor() {
 		final TextField nameField = new TextField("Parent name");
-		final Div error = new Div();
-		dialog = new Dialog();
-		dialog.add(new Label("At least one child needed"));
-		error.getElement().getStyle().set("color", "red");
-		form = new VerticalLayout(nameField, error, dialog, this.childrenGrid);
+
+		String errorMsg = "At least one child needed";
+		
+		final Div errorDiv = new Div();
+		errorDiv.setText(errorMsg);
+		errorDiv.getElement().getStyle().set("color", "red");
+		errorDiv.setVisible(false);
+
+		Dialog errorDialog = new Dialog();
+		errorDialog.add(new Label(errorMsg));
+
+		VerticalLayout form = new VerticalLayout(nameField, errorDiv, this.childrenGrid);
 
 		final Binder<Parent> binder = new Binder<>(Parent.class);
 		binder.bind(nameField, Parent::getName, Parent::setName);
 
 		final MultiSelect<Grid<Person>, Person> multiselect = this.childrenGrid.asMultiSelect();
-		//binder.forField(multiselect).withValidator(children -> !children.isEmpty(), "At least one child needed").withStatusLabel(error).bind(Parent::getChildren, Parent::setChildren);
-		binder.forField(multiselect).withValidator(children -> !children.isEmpty(), "At least one child needed")
+		binder.forField(multiselect)
+				.withValidator(children -> !errorProne || !children.isEmpty(), "At least one child needed")
 				.withValidationStatusHandler(status -> {
 					if (status.isError()) {
-
-						dialog.open();
-					} 
+						errorDiv.setVisible(true);
+						errorDialog.open();
+					} else {
+						errorDiv.setVisible(false);
+						errorDialog.close();
+					}
 				}).bind(Parent::getChildren, Parent::setChildren);
 		return new BinderCrudEditor<>(binder, form);
 	}
