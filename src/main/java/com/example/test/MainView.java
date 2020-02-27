108530db6dde84df6404f152a0c49fcdee000a69
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2020-02-27 09:01:31 +0200

    Implement errors using Dialog

--- src/main/java/com/example/test/MainView.java
+++ src/main/java/com/example/test/MainView.java
@@ -7,9 +7,12 @@ import com.vaadin.flow.component.crud.BinderCrudEditor;
 import com.vaadin.flow.component.crud.Crud;
 import com.vaadin.flow.component.crud.CrudEditor;
 import com.vaadin.flow.component.crud.CrudEditorPosition;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.Grid.SelectionMode;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
@@ -21,16 +24,25 @@ import com.vaadin.flow.router.Route;
 public class MainView extends VerticalLayout {
 
 	Grid<Person> childrenGrid;
+	VerticalLayout form;
+	Dialog dialog;
 
 	public MainView() {
 
 		final ParentDataProvider dataProvider = new ParentDataProvider();
 		setupChildrenGrid(dataProvider);
 
-		final Crud<Parent> crud = new Crud<>(Parent.class, createParentEditor());
-
+		final Crud<Parent> crud = new Crud<>(Parent.class, createParentEditor());		
+		
 		crud.setDataProvider(dataProvider);
-		crud.addSaveListener(e -> dataProvider.persist(e.getItem()));
+		crud.addSaveListener(e -> {
+			dataProvider.persist(e.getItem());
+			dialog.close();
+			if(dialog.getParent().isPresent())
+				dialog.getParent().ifPresent(p -> System.out.println(p.toString()));
+			else 
+				System.out.println("Dialog has no parent");
+		});
 		crud.addDeleteListener(e -> dataProvider.delete(e.getItem()));
 
 		crud.setEditorPosition(CrudEditorPosition.ASIDE);
@@ -45,16 +57,23 @@ public class MainView extends VerticalLayout {
 	private CrudEditor<Parent> createParentEditor() {
 		final TextField nameField = new TextField("Parent name");
 		final Div error = new Div();
+		dialog = new Dialog();
+		dialog.add(new Label("At least one child needed"));
 		error.getElement().getStyle().set("color", "red");
-		final VerticalLayout form = new VerticalLayout(nameField, error, this.childrenGrid);
+		form = new VerticalLayout(nameField, error, dialog, this.childrenGrid);
 
 		final Binder<Parent> binder = new Binder<>(Parent.class);
 		binder.bind(nameField, Parent::getName, Parent::setName);
 
 		final MultiSelect<Grid<Person>, Person> multiselect = this.childrenGrid.asMultiSelect();
+		//binder.forField(multiselect).withValidator(children -> !children.isEmpty(), "At least one child needed").withStatusLabel(error).bind(Parent::getChildren, Parent::setChildren);
 		binder.forField(multiselect).withValidator(children -> !children.isEmpty(), "At least one child needed")
-		.withStatusLabel(error).bind(Parent::getChildren, Parent::setChildren);
+				.withValidationStatusHandler(status -> {
+					if (status.isError()) {
 
+						dialog.open();
+					} 
+				}).bind(Parent::getChildren, Parent::setChildren);
 		return new BinderCrudEditor<>(binder, form);
 	}
 
